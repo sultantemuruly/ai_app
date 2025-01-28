0988abd68f9aae02147deb604fd57c45337adca3
sultantemuruly <sultantemuruly@gmail.com>
2025-01-28 22:37:35 +0500

    Created Sidebar and Header components

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,9 @@
 // Protected
 
-import { UserButton } from "@clerk/nextjs";
-
 const DashboardPage = () => {
   return (
     <>
       <div>DashboardPage</div>
-      <UserButton afterSignOutUrl="/" />
     </>
   );
 };
